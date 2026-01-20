190980c0f585cfb18bfdb9e5f8ada47e8373e248
feli08002 <feli08002@E-PW05SJ7G>
2026-01-20 22:16:32 +0100

    Add project files.

---
+++ uppgift 6.7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uppgift_6._7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(ÄrPrimtal(23));
+        }
+
+        static bool ÄrPrimtal(int tal)
+        {
+            if (tal < 2) return false;
+
+            for (int i = 2; i < tal; i++)
+                if (tal % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
+
